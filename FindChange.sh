2ec3f46ba97033a607f3e22acd3703372067a10c
Efe Ertekin <47754084+Vigilante07@users.noreply.github.com>
2019-12-23 17:30:57 +0300

    Update FindChange.sh

--- FindChange.sh
+++ FindChange.sh
@@ -5,22 +5,23 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "Example dir: $DIR"
 echo ""
 
-#Checks if there are enough arguments.
-if [ $# -ne 3 ]; then
- echo "You should give 2 arguments. First one should be '-C' or '-D'. '-C' is looking your current dir, '-D' is looking your current dir and your current dir's directories recursively. Second argument should be the word that you want to find."
-fi
 
-if [ $1 = "-C" ]; then
+#Checks if there are enough arguments.
+if [ $# -ne 2 ]; then
+ echo "You should give 2 arguments. First one should be '-C' or '-D'. '-C' is looking your current dir, '-D' is looking your current dir and your current dir's directories recursively. Second argument should be the word that you want to find."
+ else
+
+
+# -C CHECKS ONE DIRECTORY.
+  if [ $1 = "-C" ]; then
  echo "Give a directory to perform your search:"
  read -r dir1
- cd $dir1 | grep --color=always -wHn "$2" *\.*
+ cd $dir1 | grep --color=always -wHn "$2" *\.* #GETS DIR FROM USER AND TRYING TO FIND THE WORD
  declare -i x=`cd $dir1 | grep --color=always -wHn "$2" *\.* | wc -l`
   if [ $x -eq 0 ]; then
    echo "'$2' couldn't found in your current directory. "
-   exit 0
-  fi
- 
- echo "Do you want to make changes to the word you choose?[y/n]"
+   else
+     echo "Do you want to make changes to the word you choose?[y/n]"
  read -r b
  if [ $b = "y" ]; then
   echo "New coming word:"
@@ -32,18 +33,19 @@ if [ $1 = "-C" ]; then
   read -r linenum
   sed -i "$linenum s/$2/$a/g" $path
  fi
+  fi
 
+
+# -D CHECKS DIRECTORIES RECURSIVELY
 elif [ $1 = "-D" ]; then
  echo "Give a directory to perform your search:"
  read -r dir2
- cd $dir2 | grep --color=always -rwHn "$2" ${DIR}
+ cd $dir2 | grep --color=always -rwHn "$2" ${DIR} #GETS DIR FROM USER AND TRYING TO FIND THE WORD
  declare -i x=`cd $dir2 | grep --color=always -rwHn "$2" ${DIR} | wc -l`
   if [ $x -eq 0 ]; then
    echo "'$2' couldn't found in your tree of your current directory."
-   exit 0
-  fi
-
- echo "Do you want to make changes to the word you choose?[y/n]"
+   else
+    echo "Do you want to make changes to the word you choose?[y/n]"
  read -r b
  if [ $b = "y" ]; then
   echo "New word:"
@@ -53,9 +55,11 @@ elif [ $1 = "-D" ]; then
   echo "Line number of word to change:"
   read -r linenum
   sed -i "$linenum s/$2/$a/g" $path
-
  fi
+  fi
+
 
 else
  echo 'First argument is wrong! You are allowed to use "-C" or "-D".'
+fi
 fi
