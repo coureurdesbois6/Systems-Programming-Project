4549499083b275e2db4f4c73ee570790c915c37a
coureurdesbois6 <coureurdesbois6@gmail.com>
2019-12-23 22:17:43 +0300

    Update pshell.c

--- pshell.c
+++ pshell.c
@@ -61,10 +61,9 @@ int execute(char** args) {
     for(int i = 0; i < NUM_OF_CMDS; i++) {
         if(strcmp(CMDS[i], args[0]) == 0) {
             (*commands[i])(args);
-        } else {
-            printf("%s: command not found\n", args[0]);
+            return 0;
         }
-        
     }
+    printf("%s: command not found\n", args[0]);
     return 0;
 }
