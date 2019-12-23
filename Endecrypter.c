030498eace03caceddbc04be7e91ed15f267b52b
Efe Ertekin <47754084+Vigilante07@users.noreply.github.com>
2019-12-23 16:40:29 +0300

    Update Endecrypter.c

--- Endecrypter.c
+++ Endecrypter.c
@@ -9,32 +9,21 @@ int endecrypter(char **args){
         i++;
     }
 
-    
-    
-    /*char key[] = "MEF";
-    char newKey[Num_bytes+1];
-    int i,j;
-    for(i,j = 0; i<Num_bytes; i++,j++ ){
-        if(j == Num_bytes)
-            j = 0;
-        newKey[i] = key[i];
-    }
-    newKey[i] = '\0';*/
-
- 
-
     if (i != 3)
     {
         printf("YOU HAVE TO GIVE 2 ARGUMENTS! FIRST ONE FILE NAME, SECOND ONE OPERATION!\n");
-        exit (0);
     }
+    else
+    {
 
     FILE *num;
     num = fopen(args[1], "r");
     if(num == NULL){
         printf("File couldn't found\n");
-        exit (0);
     }
+    else
+    {
+    
     int Num_bytes;
     for(Num_bytes = 0; fgetc(num) != EOF; ++Num_bytes);
     fclose(num);
@@ -49,9 +38,8 @@ int endecrypter(char **args){
 
     if(file_main == NULL){
         printf("File couldn't found\n");
-        exit (0);
     }
-
+    else{
     while(1){
         ch = fgetc(file_main);
         if(ch==EOF){
@@ -65,18 +53,7 @@ int endecrypter(char **args){
         
     }
 
-    /*for (i = 0; i < Num_bytes; ++i){
-        ch = fgetc(file_main);
-        if(ch == EOF){
-            break;
-        }
-        else
-        {
-            tolower(ch);
-            ch = ((ch + newKey[i]) % 26) + 'a';
-        }
-        
-    }*/
+  
 
     fclose(file_main);
     fclose(temp);
@@ -108,12 +85,13 @@ int endecrypter(char **args){
 
     if(file_main == NULL){
         printf("File couldn't found.\n");
-        exit(0);
     }
+    else{
+
     if(temp == NULL){
         printf("File couldn't found.\n");
-        exit(0);
     }
+    else{
 	
 	while(1)
 	{
@@ -128,18 +106,7 @@ int endecrypter(char **args){
 		}
 	}
     
-    /*for (i = 0; i < Num_bytes; ++i){
-        ch = fgetc(file_main);
-        if(ch == EOF){
-            break;
-        }
-        else
-        {
-            tolower(ch);
-            ch = (((ch - newKey[i])+26) % 26) + 'a';
-        }
-        
-    }*/
+
 
 	fclose(file_main);
 	fclose(temp);
@@ -147,10 +114,17 @@ int endecrypter(char **args){
     }
     if(strcmp(args[2], "de") != 0 && strcmp(args[2], "en") != 0){
         printf("You have to give 'en' for encrypt, 'de' for decrypt as a second argument!\n");
-	exit (0);
     }
+    else
+    {
 
    printf("Size of file is %d bytes\n", Num_bytes);
 
    return 0; 
+}
+}
+}
+}
+}
+}
 }
